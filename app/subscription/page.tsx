d9aeba8c72da03df7e23d222ff1f491bae935f9f
Rubens Alves Lourenco <rubens.a.lourenco@befly.com.br>
2024-11-06 16:37:56 -0300

    chore: add shadcn setup

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,10 @@
 const Subscription = () => {
-    return (
-        <div>
-            <h1>Subscription Page</h1>
-            <p>Welcome to the subscription page.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Subscription Page</h1>
+      <p>Welcome to the subscription page.</p>
+    </div>
+  );
 };
 
 export default Subscription;
