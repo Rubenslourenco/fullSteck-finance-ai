ba88033c9362138c0e6835b9c0589af93f550bba
Rubens Alves <alvesrubens926@gmail.com>
2024-11-05 23:15:20 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,10 @@
+const Subscription = () => {
+    return (
+        <div>
+            <h1>Subscription Page</h1>
+            <p>Welcome to the subscription page.</p>
+        </div>
+    );
+};
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,10 @@
+const Transactions = () => {
+    return (
+        <div>
+            <h1>Transactions</h1>
+            <p>Welcome to the transactions page.</p>
+        </div>
+    );
+};
+
+export default Transactions;
